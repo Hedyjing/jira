fffefaf98c294834aa93a3ade8f03fc36441b22b
LAPTOP-2T7DJO2V\HanJing <jingha2@cisco.com>
2021-09-08 17:48:54 +0800

    finish search-panel debounce

--- src/screens/project-lis/index.jsx
+++ src/screens/project-lis/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { List } from "./list"
 import { SearchPanel } from "./search-panel"
 import { useEffect, useState } from "react";
-import { cleanObject } from 'utils/index';
+import { cleanObject, useDebounce, useMount } from 'utils/index';
 import * as qs from "qs";
 
 const apiUrl = process.env.REACT_APP_API_URL
@@ -12,22 +12,23 @@ export const ProjectListScreen = () => {
     name: '',
     personId: ''
   });
+  const debouncedParam = useDebounce(param, 2000);
   const [list, setList] = useState([]);
   useEffect(() => {
     // name=${param.name}&personId=${param.personId}
-    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(async response => {
+    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`).then(async response => {
       if (response.ok){
         setList(await response.json());
       }
     })
-  },[param]);
-  useEffect(() => {
+  },[debouncedParam]);
+  useMount(() => {
     fetch(`${apiUrl}/users`).then(async response => {
       if (response.ok) {
         setUsers(await response.json());
       }
     })
-  },[])
+  })
   return <div>
     <SearchPanel users={users} param={param} setParam={setParam}/>
     <List users={users} list={list}/>
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,5 @@
+import {useState, useEffect } from "react"
+
 export const isFalsy = (value) => value === 0 ? false : !value
 
 // 在一个函数里, 改变传入的对象本身是不好的
@@ -12,4 +14,36 @@ export const cleanObject = (object) => {
     }
   })
   return result;
+}
+
+export const useMount = (callback) => {
+  useEffect(() => {
+    callback();
+  }, [])
+}
+
+// 利用了闭包， log保留了timeout的值。三个log执行完后5秒后打印call
+// const debounce = (func, delay) => {
+//   let timeout;
+//   return (...param) => {
+//     if (timeout) {
+//       clearTimeout(timeout);
+//     }
+//     timeout = setTimeout(function() {
+//       func(...param);
+//     }, delay);
+//   }
+// }
+// const log = debounce(() => console.log("call"), 5000)
+// log();
+// log();
+// log();
+
+export const useDebounce = (value, delay) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+  useEffect(() => {
+    const timeout = setTimeout(() => setDebouncedValue(value),delay);
+    return () => clearTimeout(timeout);
+  }, [value, delay]);
+  return debouncedValue;
 }

50af56401c6e77ed975e5c85d227afaf67be72f9
Hedyjing <374884275@qq.com>
2021-09-09 23:04:59 +0800

    安装npx imooc-jira-tool

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {ProjectListScreen} from './screens/project-lis/index';
+import {loadDevTools} from 'jira-dev-tool'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App/>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+loadDevTools(() => 
+  ReactDOM.render(
+    <React.StrictMode>
+      <App/>
+    </React.StrictMode>,
+    document.getElementById('root')
+  )
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
